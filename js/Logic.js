bafb8563db404b08065af4f8e8dcef5eb845248e
John Doe <gabrifurquim@gmail.com>
2021-06-16 13:37:28 -0300

    planos

--- js/Logic.js
+++ js/Logic.js
@@ -10,9 +10,9 @@ let hunger = document.getElementsByClassName("hunger");
 // });
 
 // Sortear entre 2 pets ao carregar a pagina
-// Contador para diminuir pontos de fome por segundo
+// Contador para diminuir pontos de fome por segundo(iniciar ao carregar pagina)
 // Interação entre clicar em comida e aumentar contador de fome
 // Contador para diminuir ponto de saúde por segundo quando fome = 0
-// Alterar background-image para doente quando fome = 0 e saude diminuindo
-// Alterar background-image para morto quando ponto de saude = 0
+// Alterar background-image para doente quando fome = 0 e saude diminuindo (talvez usando classes em css)
+// Alterar background-image para morto quando ponto de saude = 0 (talvez usando classes em css)
 // Bonus: Sons/música, função de carinho, tela inicial, dialogo, barras de saúde e fome
