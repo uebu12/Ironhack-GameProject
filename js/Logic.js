a078e516427fcf719def941fec644108ce40b738
John Doe <gabrifurquim@gmail.com>
2021-06-16 10:24:12 -0300

    help

--- js/Logic.js
+++ js/Logic.js
@@ -1,1 +1,10 @@
-const pets = [{ name: "placeholder", img: "../img/pug.png" }];
+const pets = [{ name: "placeholder", img: "../img/Feliz.png" }];
+
+// window.addEventListener("load", )
+
+let foods = document.getElementsByClassName("foods");
+let hunger = document.getElementsByClassName("hunger").innerText;
+
+foods.addEventListener("click", () => {
+  return hunger + 100;
+});
