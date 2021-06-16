a23170073b27d353988d8e409b3c8b67ba46752a
John Doe <gabrifurquim@gmail.com>
2021-06-16 16:06:34 -0300

    quase la

--- js/Class.js
+++ js/Class.js
@@ -1,12 +1,35 @@
-class Pets {
-  constructor(petNum, petImg) {
+class Pet {
+  constructor(name, petImg) {
     this.hp = 100;
     this.hungerPoints = 100;
-    this.name = petNum.name;
+    this.name = name;
     this.petImg = petImg;
+    this.hungerInterval;
+    this.healthInterval;
   }
+
   checkIfDead() {
     return this.hp <= 0;
+  }
+  startHungerInterval(callbackHunger, callbackHealth) {
+    this.hungerInterval = setInterval(() => {
+      if (this.hungerPoints > 0) {
+        this.hungerPoints -= 20;
+        callbackHunger();
+      } else {
+        if (this.healthInterval === undefined) {
+          this.startHealthInterval(callbackHealth);
+        }
+      }
+    }, 1000);
+  }
+  startHealthInterval(callback) {
+    this.healthInterval = setInterval(() => {
+      if (this.hp > 0) {
+        this.hp -= 10;
+        callback();
+      }
+    }, 1000);
   }
 }
 
--- js/Logic.js
+++ js/Logic.js
@@ -1,19 +1,36 @@
-const pets = [{ name: "placeholder", img: "../img/Feliz.png" }];
-
-// window.addEventListener("load", )
-
 const foods = document.getElementById("foods");
-let hunger = document.getElementById("hunger").innerText;
+let hunger = document.getElementById("hunger");
+let hp = document.getElementById("hp");
+
+const pets = [
+  { name: "Pipa", img: "../img/Feliz.png" },
+  { name: "Mushi", img: "../img/Mushi sentado.png" },
+];
+const randomIndex = Math.floor(Math.random() * pets.length);
+let pet = new Pet(pets[randomIndex].name, pets[randomIndex].img);
+
+function attHunger() {
+  hunger.innerText = pet.hungerPoints;
+}
+
+function attHp() {
+  hp.innerText = pet.hp;
+}
+
+window.addEventListener("load", () => {
+  pet.startHungerInterval(attHunger, attHp);
+});
 
 foods.addEventListener("click", () => {
-  return (hunger = hunger + 100);
+  pet.hungerPoints += 10;
+  attHunger();
 });
 
 // Sortear entre 2 pets ao carregar a pagina
 // Exibir nome do pet
-// Contador para diminuir pontos de fome por segundo(iniciar ao carregar pagina)
+
 // Interação entre clicar em comida e aumentar contador de fome
-// Contador para diminuir ponto de saúde por segundo quando fome = 0
+
 // Alterar background-image para doente quando fome = 0 e saude diminuindo (talvez usando classes em css)
 // Alterar background-image para morto quando ponto de saude = 0 (talvez usando classes em css)
 // Bonus: Sons/música, função de carinho/dormir, tela inicial, dialogo, barras de saúde e fome
