9e6445ac9e7b34afdb7a1d467e34737dcb93533e
gabriel <gabrifurquim@gmail.com>
2024-01-08 09:58:06 -0300

    refatorando

--- js/Class.js
+++ js/Class.js
@@ -1,11 +1,82 @@
+
+class Game{
+  constructor(petPlacement, petName, hunger, hp){
+    this.list = [
+      { name: "Pipa", img: "../img/Feliz.png" , artist: "vampeluso"},
+      { name: "Mushi", img: "../img/Mushi sentado.png", artist:"likanakaza" },
+    ];
+    this.randomIndex = Math.floor(Math.random() * this.list.length);
+    this.pet = new Pet(this.list[this.randomIndex].name, this.list[this.randomIndex].img, this.list[this.randomIndex].artist);
+    this.petPlacement = petPlacement
+    this.petName = petName
+    this.hunger = hunger
+    this.hp = hp
+  }
+  //Atualizar sprite e nome pro pet sorteado
+
+  petSprite() {
+    if (this.pet.name === "Pipa") {
+      this.petPlacement.classList.add("petPipa");
+      this.petName.classList.add("nameAjst2");
+      this.petName.innerText = "Pipa & Pipo";
+      return;
+    } else {
+      this.petPlacement.classList.add("petMushi");
+      this.petName.classList.add("nameAjst");
+      this.petName.innerText = "Mushi";
+      return;
+    }
+  }
+
+  petStatus() {
+    if (this.pet.name === "Pipa") {
+      if (this.pet.hp < 100 && this.pet.hp > 0 && this.pet.hungerPoints === 0) {
+        this.petPlacement.classList.remove("petPipa");
+        this.petPlacement.classList.add("petPipaSick");
+        console.log("petHp < 100 && petHp > 0 && petHunger === 0");
+      } else if (this.pet.hp < 100 && this.pet.hungerPoints > 0) {
+        this.petPlacement.classList.remove("petPipaSick");
+        this.petPlacement.classList.add("petPipa");
+        console.log("petHp < 100 && petHunger > 0");
+      } else if (this.pet.hp === 0) {
+        this.petPlacement.classList.remove("petPipaSick");
+        this.petPlacement.classList.remove("petPipa");
+        this.petPlacement.classList.add("petPipaDead");
+      }
+    } else if (this.pet.name === "Mushi") {
+      if (this.pet.hp < 100 && this.pet.hp > 0 && this.pet.hungerPoints === 0) {
+        this.petPlacement.classList.remove("petMushi");
+        this.petPlacement.classList.add("petMushiSick");
+      } else if (this.pet.hp < 100 && this.pet.hungerPoints > 0) {
+        this.petPlacement.classList.remove("petMushiSick");
+        this.petPlacement.classList.add("petMushi");
+      } else if (this.pet.hp === 0) {
+        this.petPlacement.classList.remove("petMushiSick");
+        this.petPlacement.classList.remove("petMushi");
+        this.petPlacement.classList.add("petMushiDead");
+      }
+    }
+  }
+  attHunger() {
+    this.hunger.innerText = this.pet.hungerPoints;
+  }
+  attHp() {
+    this.hp.innerText = this.pet.hp;
+  }
+
+}
+
+
+
 class Pet {
-  constructor(name, petImg) {
+  constructor(name, petImg, artist) {
     this.hp = 100;
     this.hungerPoints = 100;
     this.name = name;
     this.petImg = petImg;
     this.hungerInterval;
     this.healthInterval;
+    this.artist = artist
   }
 
   checkIfDead() {
@@ -36,4 +107,11 @@ class Pet {
     }, 1000);
   }
 }
+// class Food{
+//   constructor(artist){
+// this.artist = artist
+
+//   }
+
+// }
 
--- js/Logic.js
+++ js/Logic.js
@@ -1,81 +1,86 @@
 const foods = document.getElementById("foods");
-let hunger = document.getElementById("hunger");
-let hp = document.getElementById("hp");
-let mySound;
-const pets = [
-  { name: "Pipa", img: "../img/Feliz.png" },
-  { name: "Mushi", img: "../img/Mushi sentado.png" },
-];
-const randomIndex = Math.floor(Math.random() * pets.length);
-let pet = new Pet(pets[randomIndex].name, pets[randomIndex].img);
+ let hunger = document.getElementById("hunger");
+ let hp = document.getElementById("hp");
+// const pets = [
+//   { name: "Pipa", img: "../img/Feliz.png" , artist: "vampeluso"},
+//   { name: "Mushi", img: "../img/Mushi sentado.png", artist:"likanakaza" },
+// ];
+// const randomIndex = Math.floor(Math.random() * pets.length);
+// let pet = new Pet(pets[randomIndex].name, pets[randomIndex].img);
 
-let petPlacement = document.getElementById("pet");
-let petName = document.getElementById("nome");
+ let petPlacement = document.getElementById("pet");
+ let petName = document.getElementById("nome");
+
 //Atualizar sprite e nome pro pet sorteado
-function petSprite(pet) {
-  if (pet.name === "Pipa") {
-    petPlacement.classList.add("petPipa");
-    petName.classList.add("nameAjst2");
-    petName.innerText = "Pipa & Pipo";
-    return;
-  } else {
-    petPlacement.classList.add("petMushi");
-    petName.classList.add("nameAjst");
-    petName.innerText = "Mushi";
-    return;
-  }
-}
+
+// function petSprite(pet) {
+//   if (pet.name === "Pipa") {
+//     petPlacement.classList.add("petPipa");
+//     petName.classList.add("nameAjst2");
+//     petName.innerText = "Pipa & Pipo";
+//     return;
+//   } else {
+//     petPlacement.classList.add("petMushi");
+//     petName.classList.add("nameAjst");
+//     petName.innerText = "Mushi";
+//     return;
+//   }
+// }
+
 // Atualizar sprite dependendo do hp e hunger
-function petStatus(petHp, petHunger) {
-  if (pet.name === "Pipa") {
-    if (petHp < 100 && petHp > 0 && petHunger === 0) {
-      petPlacement.classList.remove("petPipa");
-      petPlacement.classList.add("petPipaSick");
-      console.log("petHp < 100 && petHp > 0 && petHunger === 0");
-    } else if (petHp < 100 && petHunger > 0) {
-      petPlacement.classList.remove("petPipaSick");
-      petPlacement.classList.add("petPipa");
-      console.log("petHp < 100 && petHunger > 0");
-    } else if (petHp === 0) {
-      petPlacement.classList.remove("petPipaSick");
-      petPlacement.classList.remove("petPipa");
-      petPlacement.classList.add("petPipaDead");
-    }
-  } else if (pet.name === "Mushi") {
-    if (petHp < 100 && petHp > 0 && petHunger === 0) {
-      petPlacement.classList.remove("petMushi");
-      petPlacement.classList.add("petMushiSick");
-    } else if (petHp < 100 && petHunger > 0) {
-      petPlacement.classList.remove("petMushiSick");
-      petPlacement.classList.add("petMushi");
-    } else if (petHp === 0) {
-      petPlacement.classList.remove("petMushiSick");
-      petPlacement.classList.remove("petMushi");
-      petPlacement.classList.add("petMushiDead");
-    }
-  }
-}
 
-function attHunger() {
-  hunger.innerText = pet.hungerPoints;
-  if (pet.hp === 0) {
-  }
-}
+// function petStatus(petHp, petHunger) {
+//   if (pet.name === "Pipa") {
+//     if (petHp < 100 && petHp > 0 && petHunger === 0) {
+//       petPlacement.classList.remove("petPipa");
+//       petPlacement.classList.add("petPipaSick");
+//       console.log("petHp < 100 && petHp > 0 && petHunger === 0");
+//     } else if (petHp < 100 && petHunger > 0) {
+//       petPlacement.classList.remove("petPipaSick");
+//       petPlacement.classList.add("petPipa");
+//       console.log("petHp < 100 && petHunger > 0");
+//     } else if (petHp === 0) {
+//       petPlacement.classList.remove("petPipaSick");
+//       petPlacement.classList.remove("petPipa");
+//       petPlacement.classList.add("petPipaDead");
+//     }
+//   } else if (pet.name === "Mushi") {
+//     if (petHp < 100 && petHp > 0 && petHunger === 0) {
+//       petPlacement.classList.remove("petMushi");
+//       petPlacement.classList.add("petMushiSick");
+//     } else if (petHp < 100 && petHunger > 0) {
+//       petPlacement.classList.remove("petMushiSick");
+//       petPlacement.classList.add("petMushi");
+//     } else if (petHp === 0) {
+//       petPlacement.classList.remove("petMushiSick");
+//       petPlacement.classList.remove("petMushi");
+//       petPlacement.classList.add("petMushiDead");
+//     }
+//   }
+// }
 
-function attHp() {
-  hp.innerText = pet.hp;
-}
+// function attHunger() {
+//   hunger.innerText = pet.hungerPoints;
+//   if (pet.hp === 0) {
+//   }
+// }
+
+// function attHp() {
+//   hp.innerText = pet.hp;
+// }
+
+const game = new Game(petPlacement, petName, hunger, hp)
 
 window.addEventListener("load", () => {
-  pet.startHungerInterval(attHunger, attHp, petStatus);
-  petSprite(pet);
+  game.pet.startHungerInterval(game.attHunger, game.attHp, game.petStatus);
+  game.petSprite();
 });
 
 foods.addEventListener("click", () => {
-  if (pet.hp > 0) {
-    pet.hungerPoints += 40;
+  if (game.pet.hp > 0) {
+    game.pet.hungerPoints += 40;
 
-    attHunger();
+    game.attHunger();
   }
 });
 
